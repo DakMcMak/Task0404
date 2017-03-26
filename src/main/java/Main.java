cad8a59b3113962895f831661db143febc26dd3e
DakMcMak <esbolov.galymzhan@mail.ru>
2017-03-26 23:11:35 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    private static int catsCount = 0;
+
+    public static void addNewCat() {
+        catsCount++;
+    }
+
+    public static void main(String[] args) {
+    }
+}
